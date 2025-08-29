7d9618494273b564641a306306ba43a33c217582
Dmitrii Pavlov <dm.rom.pav@gmail.com>
2025-08-30 01:54:04 +0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // When deploying to GitHub Pages, set the base path via GH_PAGES_BASE env var
-  base: process.env.GH_PAGES_BASE || undefined,
+  base: "dex",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});

d6937b34159851b41927a33d5a62a045baf631c1
Dmitrii Pavlov <dm.rom.pav@gmail.com>
2025-08-30 01:59:47 +0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "dex",
+  base: "/dex/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
